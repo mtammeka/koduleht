47b80c76b8e7073591eb1fda6b75fcdadd202ab8
Madis Tammekänd <mtammekand@gmail.com>
2017-02-26 15:11:59 +0200

    tabeliharjutus

--- index.php
+++ index.php
@@ -19,7 +19,8 @@
 	<img src="pildid/c.gif" alt="..."/>
 	<p>
 		<a href="esimene_n2dal.html"><h1>i244 esimese nädala nädalatöö - Sissejuhatus HTML-i</h1></a>
-		<a href="teine_n2dal.html"><h1>i244 teise nädala nädalatöö - Peenem HTML</h1></a>
+		<h1>i244 teise nädala nädalatöö - Peenem HTML</h1>
+		<a href="tabeliharjutus.html"><h1>&nbsp;&nbsp;&nbsp;&nbsp;1.) tabeliharjutus</h1></a>
 	</p>
 	<p id="suvajutt">Tervitus!</p>
 
