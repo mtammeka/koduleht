59ce892dd640959440263b5c62d8ab09857be1f4
Madis Tammekänd <mtammekand@gmail.com>
2017-04-02 18:31:36 +0300

    php valmis

--- seitsmes_n2dal/index.php
+++ seitsmes_n2dal/index.php
@@ -10,36 +10,41 @@
 		<title>
       PHP
     </title>
+
     <style type="text/css">
       .active { background: skyblue; padding: 10px;}
     </style>
+    
     <script type="text/javascript">
   
       window.addEventListener("DOMContentLoaded", function() {
+    
         console.log("Tere!");
+    
       });
 
     </script>
+
 	</head>
+  
   <body>
   
     <div id="content">
     	<?php
+        $tekst = "Test!";	
+        $reversed = ""; // pole tingimata vist vajalik
+
+        echo "Tekst: $tekst <br>";
+
+        for ($i = strlen($tekst) - 1; $i >= 0; $i -= 1) {
+          $reversed = $reversed . $tekst[$i];
+        }
+
+        echo "Teistpidi: $reversed <br>";
         
-        echo '<p>Hello World</p>';
-
-        for ($i = 10; $i > 0; $i -= 1) {
-          echo "$i ... ";
-        } 
-
-        $version = phpversion();
-        print "<br"> + $version;
-        print "<br>";
-
-        /* aga võib ka nii */
-      
-        print PHP_VERSION;
       ?>
+
     </div>
+  
   </body>
 </html>
